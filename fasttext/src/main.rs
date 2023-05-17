4980e3954f01ecdc62c5af3dd6a3b97c411f4cf0
kampersanda <shnsk.knd@gmail.com>
2023-05-17 23:33:15 +0900

    add

---
+++ fasttext/src/main.rs
@@ -0,0 +1,19 @@
+use std::fs::File;
+use std::io::BufReader;
+
+use finalfusion::prelude::*;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    let mut reader = BufReader::new(File::open(&args[1]).unwrap());
+
+    // Read a file in .bin.
+    let embeddings = Embeddings::read_fasttext(&mut reader).unwrap();
+
+    // Look up an embedding.
+    let embedding = embeddings.embedding("zwei");
+
+    // Print the embedding.
+    println!("{:?}", embedding);
+}
