db4a0ff338e73b49ba78ebb85a5c50ea4a786342
Shunsuke Kanda <shnsk.knd@gmail.com>
2023-06-03 00:10:45 +0900

    Add embedding tools (#23)
    * add
    
    * add
    
    * add

---
+++ sif-tools/README.md
@@ -0,0 +1,21 @@
+# sif-embedding/sif-tools
+
+This directory provides tools to perform sentence embedding using sif-embedding.
+
+## Preparation
+
+sif-embedding, or the SIF algorithm, requires (pre-trained) word embeddings and unigram probabilities.
+You first need to prepare them.
+
+For word embeddings, [finalfusion-tools](../finalfusion-tools) provides instructions to download and compile well-known pre-trained word embeddings such as GloVe or fastText.
+For unigram probabilities, [wordfreq-tools](../wordfreq-tools) provides instructions to download and compile unigram probabilities from data provided in wordfreq.
+
+## Sentence embedding
+
+Sentence embeddings can be performed using the models prepared in the above steps.
+
+```
+$ echo "hello i am\ngood morning" | cargo run --bin embedding -- -f path/to/glove.42B.300d.fifu -u path/to/large_en.unigram
+0.0037920314 -0.018138476 0.010073537 ... -0.002471894
+-0.0029124343 0.013930968 -0.0077368026 ... 0.001898489
+```
---
+++ sif-tools/src/embedding.rs
@@ -0,0 +1,57 @@
+// These declarations are required to recognize the backend.
+// https://github.com/rust-ndarray/ndarray-linalg/blob/ndarray-linalg-v0.16.0/lax/src/lib.rs
+#[cfg(any(feature = "intel-mkl-static", feature = "intel-mkl-system"))]
+extern crate intel_mkl_src as _src;
+#[cfg(any(feature = "netlib-static", feature = "netlib-system"))]
+extern crate netlib_src as _src;
+#[cfg(any(feature = "openblas-static", feature = "openblas-system"))]
+extern crate openblas_src as _src;
+
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
+use std::path::PathBuf;
+
+use clap::Parser;
+use finalfusion::prelude::*;
+
+use sif_embedding::{Sif, UnigramLM};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short = 'f', long)]
+    input_fifu: PathBuf,
+
+    #[arg(short = 'u', long)]
+    input_unigram: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let word_embeddings = {
+        let mut reader = BufReader::new(File::open(&args.input_fifu)?);
+        Embeddings::<VocabWrap, StorageWrap>::mmap_embeddings(&mut reader)?
+    };
+    let unigram_lm = {
+        let mut reader = BufReader::new(File::open(&args.input_unigram)?);
+        let mut data = vec![];
+        reader.read_to_end(&mut data)?;
+        let (unigram_lm, _) = UnigramLM::deserialize_from_slice(&data[..])?;
+        unigram_lm
+    };
+
+    let sif = Sif::new(&word_embeddings, &unigram_lm);
+    let sent_embeddings = sif.embeddings(std::io::stdin().lock().lines().map(|l| l.unwrap()));
+    for sent_embedding in sent_embeddings.rows() {
+        let row = sent_embedding
+            .iter()
+            .map(|f| f.to_string())
+            .collect::<Vec<_>>()
+            .join(" ");
+        println!("{row}");
+    }
+
+    Ok(())
+}
