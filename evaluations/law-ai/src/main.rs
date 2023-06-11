55ab0ec64b606c199db9da2bd3d87b0dad942a1f
kampersanda <shnsk.knd@gmail.com>
2023-06-11 11:20:43 +0900

    put

--- evaluations/law-ai/src/main.rs
+++ evaluations/law-ai/src/main.rs
@@ -3,55 +3,112 @@ extern crate openblas_src;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read};
 use std::path::{Path, PathBuf};
 
 use clap::Parser;
-use instant_segment::Search;
-use instant_segment::Segmenter;
+use finalfusion::prelude::*;
+use sif_embedding::Sif;
+use tantivy::tokenizer::*;
+use wordfreq_model::{self, ModelKind};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    // #[arg(short = 'f', long)]
-    // input_fifu: PathBuf,
+    #[arg(short = 'f', long)]
+    input_fifu: PathBuf,
 
-    // #[arg(short = 'w', long)]
-    // input_weights: PathBuf,
+    #[arg(short = 'd', long)]
+    data_dir: PathBuf,
+}
 
-    // #[arg(short = 'c', long)]
-    // corpora_dir: PathBuf,
+struct EnglishAnalyzer {
+    analyzer: tantivy::tokenizer::TextAnalyzer,
+}
+
+impl EnglishAnalyzer {
+    fn new() -> Self {
+        let en_stem = TextAnalyzer::builder(SimpleTokenizer::default())
+            .filter(RemoveLongFilter::limit(40))
+            .filter(LowerCaser)
+            .build();
+        Self { analyzer: en_stem }
+    }
+
+    fn analyze(&mut self, text: &str) -> String {
+        let mut tokens = self.analyzer.token_stream(text);
+        let mut token_texts = Vec::new();
+        while let Some(tok) = tokens.next() {
+            token_texts.push(tok.text.to_string());
+        }
+        token_texts.join(" ")
+    }
+}
+
+struct DatasetHandler {
+    document_files: Vec<PathBuf>,
+    gold_scores: Vec<(usize, usize, f32)>,
+}
+
+impl DatasetHandler {
+    fn new(data_dir: &Path) -> Self {
+        let data_dir = data_dir.to_str().unwrap();
+        let document_files = glob::glob(&format!("{data_dir}/documents/*.txt"))
+            .unwrap()
+            .filter_map(Result::ok)
+            .collect::<Vec<_>>();
+
+        let file_name_map = document_files
+            .iter()
+            .enumerate()
+            .map(|(i, file)| (file.file_stem().unwrap().to_str().unwrap().to_string(), i))
+            .collect::<HashMap<_, _>>();
+
+        let mut gold_scores = vec![];
+        let similarity_scores_reader =
+            BufReader::new(File::open(&format!("{data_dir}/similarity_scores.csv")).unwrap());
+        for line in similarity_scores_reader.lines() {
+            let line = line.unwrap();
+            let mut cols = line.split(",");
+            let file_id_1 = file_name_map[cols.next().unwrap()];
+            let file_id_2 = file_name_map[cols.next().unwrap()];
+            let score = cols.next().unwrap().parse::<f32>().unwrap();
+            gold_scores.push((file_id_1, file_id_2, score));
+        }
+
+        Self {
+            document_files,
+            gold_scores,
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // let args = Args::parse();
+    let args = Args::parse();
 
-    let unigram_reader = BufReader::new(File::open("data/en-unigrams.txt")?);
-    let bigram_reader = BufReader::new(File::open("data/en-bigrams.txt")?);
+    let dataset = DatasetHandler::new(&args.data_dir);
 
-    let unigram_iter = unigram_reader.lines().map(|line| {
-        let line = line.unwrap();
-        let mut split = line.split('\t');
-        let word = split.next().unwrap();
-        let prob = split.next().unwrap();
-        (word.into(), prob.parse::<f64>().unwrap())
-    });
-    let bigram_iter = bigram_reader.lines().map(|line| {
-        let line = line.unwrap();
-        let mut split = line.split('\t');
-        let words = split.next().unwrap();
-        let prob = split.next().unwrap();
-        let mut word_split = words.split(' ');
-        let word1 = word_split.next().unwrap();
-        let word2 = word_split.next().unwrap();
-        ((word1.into(), word2.into()), prob.parse::<f64>().unwrap())
-    });
+    let word_embeddings = {
+        let mut reader = BufReader::new(File::open(&args.input_fifu)?);
+        Embeddings::<VocabWrap, StorageWrap>::mmap_embeddings(&mut reader)?
+    };
+    let unigram_lm = wordfreq_model::load_wordfreq(ModelKind::LargeEn)?;
+    let sif = Sif::new(&word_embeddings, &unigram_lm);
 
-    let segmenter = Segmenter::from_iters(unigram_iter, bigram_iter);
-    let mut search = Search::default();
+    let mut analyzer = EnglishAnalyzer::new();
+    let sent_embeddings = sif.embeddings(dataset.document_files.iter().map(|file| {
+        let mut reader = BufReader::new(File::open(file).unwrap());
+        let mut text = String::new();
+        reader.read_to_string(&mut text).unwrap();
+        analyzer.analyze(&text)
+    }));
 
-    let words = segmenter.segment("haven't", &mut search)?;
-    println!("{:?}", words.collect::<Vec<&str>>());
+    for &(file_id_1, file_id_2, gold_score) in &dataset.gold_scores {
+        let e1 = &sent_embeddings.row(file_id_1);
+        let e2 = &sent_embeddings.row(file_id_2);
+        let score = sif_embedding::util::cosine_similarity(e1, e2).unwrap_or(0.);
+        println!("{},{},{},{}", file_id_1, file_id_2, score, gold_score);
+    }
 
     Ok(())
 }
